97b6d61ce1b8b74bcf327b0af474e483b5d12741
Tomasz Durka <tomasz@durka.pl>
2016-07-10 11:17:22 +0200

    Run manager inside vagrant box

--- puppet/environments/development/manifests/cluster.pp
+++ puppet/environments/development/manifests/cluster.pp
@@ -1,3 +1,17 @@
 node default {
   class { 'janus_cluster::node': }
+
+  require 'nodejs'
+  $port = 8080;
+  
+  user { 'janus-cluster-manager':
+    ensure  => present,
+    system  => true,
+  }
+  ->
+  daemon { 'janus-cluster-manager':
+    binary  => '/usr/bin/node',
+    args    => "/vagrant/bin/janus-cluster-manager --port ${port}",
+    user    => 'janus-cluster-manager',
+  }
 }
